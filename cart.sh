c2eca5eacdd588d85e2155e4321cd6658b55ab46
Revanthsatyam <satyamrevanth@gmail.com>
2025-07-16 18:43:49 +0530

    roboshoshell frountend

---
+++ cart.sh
@@ -0,0 +1,16 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+cp cart.repo /etc/systemd/system/cart.service
+
+useradd roboshop
+mkdir /app
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+cd /app
+unzip /tmp/user.zip
+cd /app
+npm install
+
+systemctl daemon-reload
+systemctl enable cart
+systemctl start cart
