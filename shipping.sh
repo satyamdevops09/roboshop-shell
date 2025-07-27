7885fbe3660940f7a2b2a365855a85abdd1eb87d
Revanthsatyam <satyamrevanth@gmail.com>
2025-07-27 11:48:07 +0530

    roboshoshell frountend

--- shipping.sh
+++ shipping.sh
@@ -3,6 +3,6 @@ component=shipping
 MAVEN
 
  dnf install mysql -y
-mysql -h mysql-dev.sdevops09.online -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.sdevops09.online -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql-dev.sdevops09.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql-dev.sdevops09.online -uroot -p$1 < /app/db/schema.sql
+mysql -h mysql-dev.sdevops09.online -uroot -p$1 < /app/db/app-user.sql
+mysql -h mysql-dev.sdevops09.online -uroot -p$1 < /app/db/master-data.sql
