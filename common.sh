4d47635e4a5145bd63692bb54bb5e8d04ddb34ac
Revanthsatyam <satyamrevanth@gmail.com>
2025-07-27 11:25:57 +0530

    roboshoshell frountend

--- common.sh
+++ common.sh
@@ -38,3 +38,22 @@ PYTHON(){
   SYSTEMD
 
 }
+
+MAVEN(){
+  dnf install maven -y
+  cp shipping.service /etc/systemd/system/shipping.service
+
+  useradd roboshop
+  mkdir /app
+  curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
+  cd /app
+  unzip /tmp/shipping.zip
+  cd /app
+  mvn clean package
+  mv target/shipping-1.0.jar shipping.jar
+  dnf install mysql -y
+
+  systemctl daemon-reload
+  systemctl enable shipping
+  systemctl start shipping
+}
--- shipping.sh
+++ shipping.sh
@@ -1,18 +1,8 @@
-dnf install maven -y
-cp shipping.service /etc/systemd/system/shipping.service
+source common.sh
+component=shipping
+MAVEN
 
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-dnf install mysql -y
+
 mysql -h mysql-dev.sdevops09.online -uroot -pRoboShop@1 < /app/db/schema.sql
 mysql -h mysql-dev.sdevops09.online -uroot -pRoboShop@1 < /app/db/app-user.sql
 mysql -h mysql-dev.sdevops09.online -uroot -pRoboShop@1 < /app/db/master-data.sql
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
