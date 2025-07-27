ac16afc495cbb57578eabe4f0a3b1f24488f8f93
Revanthsatyam <satyamrevanth@gmail.com>
2025-07-27 11:29:28 +0530

    roboshoshell frountend

--- common.sh
+++ common.sh
@@ -1,6 +1,5 @@
 APP_PREREQ(){
    cp ${component}.service /etc/systemd/system/${component}.service
-
     useradd roboshop
     rm -rf /app
     mkdir /app
@@ -12,41 +11,28 @@ SYSTEMD(){
     systemctl daemon-reload
     systemctl enable ${component}
     systemctl start ${component}
-
 }
 NODEJS(){
-
   dnf module disable nodejs -y
   dnf module enable nodejs:20 -y
   dnf install nodejs -y
-
-
-
  APP_PREREQ
   npm install
   SYSTEMD
-
-
-
 }
 
 PYTHON(){
   dnf install python3 gcc python3-devel -y
   APP_PREREQ
-
   pip3 install -r requirements.txt
   SYSTEMD
-
 }
 
 MAVEN(){
   dnf install maven -y
   cp ${component}.service /etc/systemd/system/${component}.service
-
   APP_PREREQ
   mvn clean package
   mv target/${component}-1.0.jar ${component}.jar
-
-
   SYSTEMD
 }
