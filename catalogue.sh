bd9e115e01044fe543c0653cb30b56b4c5c66169
Revanthsatyam <satyamrevanth@gmail.com>
2025-07-27 11:19:54 +0530

    roboshoshell frountend

--- catalogue.sh
+++ catalogue.sh
@@ -1,17 +1,6 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-useradd roboshop
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
+source common.sh
+component=catalogue
+NODEJS
+
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.sdevops09.online </app/db/master-data.js
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
--- common.sh
+++ common.sh
@@ -1,22 +1,40 @@
-NODEJS{
+APP_PREREQ(){
+   cp ${component}.service /etc/systemd/system/${component}.service
+
+    useradd roboshop
+    rm -rf /app
+    mkdir /app
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
+    cd /app
+}
+
+SYSTEMD(){
+    systemctl daemon-reload
+    systemctl enable ${component}
+    systemctl start ${component}
+
+}
+NODEJS(){
 
   dnf module disable nodejs -y
   dnf module enable nodejs:20 -y
   dnf install nodejs -y
 
-  cp ${component}.service /etc/systemd/system/${component}.service
 
-  useradd roboshop
-  rm -rf /app
-  mkdir /app
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
-  cd /app
-  unzip /tmp/${component}.zip
-  cd /app
+
+ APP_PREREQ
   npm install
+  SYSTEMD
 
 
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl start ${component}
+
+}
+
+PYTHON(){
+  dnf install python3 gcc python3-devel -y
+  APP_PREREQ
+
+  pip3 install -r requirements.txt
+  SYSTEMD
+
 }
