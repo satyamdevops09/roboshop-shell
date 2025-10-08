e430b63d0b266dc20928c07dc92e795baaa4f704
Revanthsatyam <satyamrevanth@gmail.com>
2025-10-08 18:36:58 +0530

    cart

--- shipping.sh
+++ shipping.sh
@@ -1,13 +1,15 @@
-if [ -z "$1" ]; then
-  echo Input Password is missing
-  exit 1
-fi
-source common.sh
-component=shipping
-MAVEN
+#if [ -z "$1" ]; then
+#  echo Input Password is missing
+#  exit 1
+#fi
+#source common.sh
+#component=shipping
+#MAVEN
+#
+# dnf install mysql -y
+# for file in schema app-user master-data; do
+#mysql -h mysql-dev.sdevops09.online -uroot -p$1 < /app/db/$file.sql
+#
+#done
 
- dnf install mysql -y
- for file in schema app-user master-data; do
-mysql -h mysql-dev.sdevops09.online -uroot -p$1 < /app/db/$file.sql
 
-done
